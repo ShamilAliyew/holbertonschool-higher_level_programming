8b1544deb57663ffdccb77d37c9d4eea6c789ba8
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-01 00:49:18 +0400

    Done

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for s in str:
-        if ord(s) >= ord('a') or ord(s) <= ord('z'):
+        if ord(s) >= ord('a') and ord(s) <= ord('z'):
             print('{}'.format(chr(ord(s)-32)), end='')
         else:
             print('{}'.format(s), end='')
+uppercase("Best School 98 Battery street")
