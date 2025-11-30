79fd5323ab988549a9fb8e1c97edc09b39150e7c
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-11-30 23:55:37 +0400

    Done

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(s):
-   if ord(s) <= 122 and ord(s) >= 97:
-       return True
-   else:
-       return False
+    if ord(s) <= 122 and ord(s) >= 97:
+        return True
+    else:
+        return False
