6e92336e3a1984cb8e3f37b3ece07dbc27964544
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-11-30 23:39:26 +0400

    Done

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(s):
     if not s:
-        raise Exception('Empty string not allowed')
+        raise ValueError('Empty string not allowed')
     l = []
     for i in range(93, 127):
         l.append(chr(i))
