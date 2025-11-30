7e15a160e535f95fba7f8cc0a0a9d75f2bef5f2f
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-01 00:12:35 +0400

    Done

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if type(number) != str:
+    if type(number) == str:
+        raise Exception("The input is not a string")
+    else:
         return number % 10 if number >= 0 else number % -10
