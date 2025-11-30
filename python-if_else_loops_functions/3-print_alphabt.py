1fd0963db7d8b772b48765b44767d0cf356b83a0
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-11-30 22:30:36 +0400

    Done

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(97, 123) if i != ord('q') and i != ord('e'))))
+print("{}".format("".join(chr(i) for i in range(97, 123) if i != ord('q') and i != ord('e'))),end='')
