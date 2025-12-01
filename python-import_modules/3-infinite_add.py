0723bec4c1054a3b78f77a8d99c01932d20fccf1
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-01 19:11:08 +0400

    Done

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     sumarg = 0
     for i in range(len(arguments)):
-        if arguments[i].isdigit() or (arguments[i].startswith('-') and arguments[i][1:].isdigit()):
+        if arguments[i].isdigit() or (arguments[i].startswith('-')
+                                      and arguments[i][1:].isdigit()):
             sumarg += int(arguments[i])
         else:
             continue
