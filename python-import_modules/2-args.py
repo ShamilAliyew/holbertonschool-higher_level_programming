40b11ddf81bd2cd95172f73669f574d9625db740
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-01 18:54:38 +0400

    Done

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
         print("{} arguments.".format(0))
     elif len(arguments) == 2:
         print("{} argument:\n{}: {}"
-              .format(len(arguments)-1, len(arguments)-1, arguments[len(arguments)-1]))
+              .format(len(arguments)-1, len(arguments)-1,
+                                    arguments[len(arguments)-1]))
     elif len(arguments) > 2:
         print("{} arguments:".format(len(arguments)-1))
         for i in range(len(arguments)):
