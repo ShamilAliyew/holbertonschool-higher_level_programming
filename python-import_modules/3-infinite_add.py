15f582e68ddad1fa1e65b32e6d3be3d814a80e6e
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-01 19:10:42 +0400

    Done

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import sys
+from curses.ascii import isdigit
+
 if __name__ == "__main__":
     arguments = sys.argv[1:]
     sumarg = 0
     for i in range(len(arguments)):
-        sumarg += int(arguments[i])
+        if arguments[i].isdigit() or (arguments[i].startswith('-') and arguments[i][1:].isdigit()):
+            sumarg += int(arguments[i])
+        else:
+            continue
     print(sumarg)
