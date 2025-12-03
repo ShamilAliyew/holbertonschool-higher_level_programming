5b3035fa6f68517f2233b4bbdb7c94fea4b61076
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-03 20:59:22 +0400

    Done

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
-        return 0
+        tuple_a = 0
     if len(tuple_b) > 2:
         tuple_b = tuple_b[:2]
     return tuple(x + y for x, y in zip(tuple_a, tuple_b))
