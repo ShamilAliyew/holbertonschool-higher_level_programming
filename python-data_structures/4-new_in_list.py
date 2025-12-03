62c80bd7809ac9e339b1d5e9d77f9a74a87121cb
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-03 19:08:08 +0400

    Done

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,6 +5,8 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return my_list
     else:
-        replaced_list = my_list[:]
-        replaced_list[idx] = element
-    return replaced_list
+        new_list = my_list[:]
+        new_list[idx] = element
+    return new_list
+
+
