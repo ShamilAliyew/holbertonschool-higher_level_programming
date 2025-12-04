b5694de8ffdb232a8d3e297ea8f037e1e3c8ee5e
= <=>
2025-12-04 19:12:45 +0400

    Done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or a_dictionary is None:
+    if len(a_dictionary) == 0:
+        return None
+    if a_dictionary is None:
         return None
     return max(a_dictionary, key=a_dictionary.get)
