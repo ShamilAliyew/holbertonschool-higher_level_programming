9d98d2e28cf23de612ffbba76de7a2ffdb8daf93
= <=>
2025-12-04 19:24:19 +0400

    Done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    if a_dictionary is None:
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
+
+    temp = list(a_dictionary.keys())[0]
+    max = a_dictionary[temp]
+
+    for k,v in a_dictionary.items():
+        if v > max:
+            temp = k
+            max = v
+    return temp
