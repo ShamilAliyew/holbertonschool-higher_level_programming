4cd89166dbe5c5b55ac8b88dc0e7336881c80ac8
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-08 18:34:41 +0400

    Done

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
