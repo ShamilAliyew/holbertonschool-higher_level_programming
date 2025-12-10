0be67d45c745aaf1719984b6ed6b11ddb5a68117
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-11 01:33:01 +0400

    Done

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """a function that returns True if the object is exactly an instance of the specified class ; otherwise False"""
+from _testcapi import MyList
 
 
 def is_same_class(obj, a_class):
     """a function that returns True if the object is exactly an instance of the specified class ; otherwise False"""
-    return isinstance(obj,a_class)
+    return type(obj) == a_class
