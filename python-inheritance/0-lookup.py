1c798226654c705ed247e8a444bab51fc453814a
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-10 18:53:01 +0400

    Done

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,13 +2,6 @@
 """the list of available attributes and methods of an object"""
 
 
-class MyClass:
-
-    name = "MyClass"
-    count = 1
-    def anyFunction(self):
-        pass
-
 def lookup(cls):
     """a function that returns the list of available attributes and methods of an object"""
     return cls.__dict__
