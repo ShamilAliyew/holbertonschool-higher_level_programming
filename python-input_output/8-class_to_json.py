4b4ad00c7f3d724338585106c0f1e018ab81604e
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-14 03:04:21 +0400

    Done

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,11 +2,10 @@
 """a function that returns the dictionary description
  with simple data structure (list, dictionary, string, integer and boolean)
   for JSON serialization of an object"""
-import json
 
 
 def class_to_json(obj):
     """a function that returns the dictionary description
      with simple data structure (list, dictionary, string, integer and boolean)
       for JSON serialization of an object"""
-    return json.dumps(obj, indent=4)
+    return list(obj.__dict__.values())
