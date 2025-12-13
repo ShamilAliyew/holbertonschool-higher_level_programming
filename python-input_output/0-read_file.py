59143642d3a7685bf05fed9741cea85547a4afb2
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-13 17:38:02 +0400

    Done

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout"""
+    try:
+        with open(filename, "r") as file:
+            print(file.read())
+    except Exception:
+        print("file doesn't exist")
