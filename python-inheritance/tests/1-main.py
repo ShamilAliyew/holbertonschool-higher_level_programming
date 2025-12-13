1335677e3ea3af3868765de1b68c3a33d1f72e0a
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-13 18:03:17 +0400

    Done

--- python-inheritance/tests/1-main.py
+++ python-inheritance/tests/1-main.py
@@ -1,4 +1,5 @@
-MyList = __import__('1-my_list.py').MyList
+#!/usr/bin/python3
+MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
 my_list.append(1)
@@ -6,7 +7,6 @@ my_list.append(4)
 my_list.append(2)
 my_list.append(3)
 my_list.append(5)
-
 print(my_list)
 my_list.print_sorted()
 print(my_list)
