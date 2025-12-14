6d68377536195ecde9836f8012b312b31da5ba93
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-14 17:40:27 +0400

    Done[

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""a class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if isinstance(attr, str) and hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        obj = {}
+        pairs = json.split(',')
+        for pair in pairs:
+            key, value = pair.split(':')
+            # Basic type handling
+            if value.isdigit():
+                obj[key] = int(value)
+            else:
+                obj[key] = value
+        return obj
