09090f7d2cca7dfa940bf03e69aed61e310fa796
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 14:54:48 +0400

    Done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- a script that prints the following description of the table first_table
 -- a script that prints the following description of the table first_table
-SHOW COLUMNS FROM hbtn_0c_0;
+SHOW COLUMNS FROM first_table;
