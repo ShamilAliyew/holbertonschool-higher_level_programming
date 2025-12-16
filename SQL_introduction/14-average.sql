dd0458298edfa8f3c4b39f9c044cbe4f1f3f44a3
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 15:53:18 +0400

    Done

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- a script that computes the score average of all records in the table second_table
 -- a script that computes the score average of all records in the table second_table
-SELECT AVG(SCORE)
+SELECT AVG(SCORE) as 'average'
 FROM second_table;
