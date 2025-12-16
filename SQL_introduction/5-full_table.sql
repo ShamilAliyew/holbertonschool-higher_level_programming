7c61e91b7d0da191133c9268f711446e94dbc127
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 15:06:08 +0400

    Done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,5 @@
 -- a script that prints the following description of the table first_table
 -- a script that prints the following description of the table first_table
+USE hbtn_0c_0;
 CREATE TABLE first_table(ID INT DEFAULT NULL,
                          NAME VARCHAR(256) DEFAULT NULL);
