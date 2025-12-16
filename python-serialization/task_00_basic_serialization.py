2cc6e64a7b159dc6f701761b07a1f3e82d90cb4b
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 11:46:01 +0400

    Done

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,10 +6,24 @@ import  json
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, "w") as file:
-        json.dump(data, file)
+    def serialize(obj):
+        if isinstance(obj, set):
+            return {
+                "__type__": "set",
+                "elements": list(obj)
+            }
+        return obj
+
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, default=serialize)
 
 
 def load_and_deserialize(filename):
-    with open(filename, "r") as file:
-        data = json.load(file)
+    def deserialize(obj):
+        if "__type__" in obj and obj["__type__"] == "set":
+            return set(obj["elements"])
+        return obj
+
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file, object_hook=deserialize)
+
