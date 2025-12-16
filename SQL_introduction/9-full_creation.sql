8a5ea660b4113d04a54e56bc826294fe0dae31e2
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 15:32:03 +0400

    Done

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,9 @@ CREATE TABLE IF NOT EXISTS second_table(ID INT,
                                          NAME VARCHAR(256),
                                          SCORE INT)
 INSERT INTO second_table(ID, NAME, SCORE)
-VALUES((1,"John",10),
+VALUES(
+        (1,"John",10),
         (2,"Alex",3),
         (3,"Bob",14),
-        (4,"George",8));
+        (4,"George",8)
+        );
