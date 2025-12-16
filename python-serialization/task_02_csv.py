458b503a86966d7916c215e742c70598d0a95656
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 13:41:54 +0400

    Done

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -6,14 +6,13 @@ import os.path
 def convert_csv_to_json(csv_file):
     if not os.path.exists(csv_file):
        print("file not found")
+       return False
     with open(csv_file, newline="") as f:
         read = csv.DictReader(f)
         data = list(read)
 
-    try:
-        with open("data.json", "w") as json_file:
-            json.dump(data, json_file)
+
+    with open("data.json", "w") as json_file:
+        json.dump(data, json_file)
         return True
-    except Exception as ex:
-        print(f"file not found: {ex}")
-        return False
+
