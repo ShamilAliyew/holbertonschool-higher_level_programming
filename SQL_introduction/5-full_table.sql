e58a6e72f206af6763e0560cc1b6157639e42ec6
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 15:18:46 +0400

    Done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,3 @@
 -- a script that prints the following description of the table first_table
 -- a script that prints the following description of the table first_table
-CREATE TABLE `first_table` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) DEFAULT NULL,
-  `c` char(1) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SHOW CREATE TABLE first_table;
