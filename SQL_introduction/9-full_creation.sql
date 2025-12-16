77956dd9c0bd3d5a0d251dced177f1efe2487a99
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 15:31:48 +0400

    Done

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- a script that creates a table second_table in the database hbtn_0c_0
+-- a script that creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table(ID INT,
+                                         NAME VARCHAR(256),
+                                         SCORE INT)
+INSERT INTO second_table(ID, NAME, SCORE)
+VALUES((1,"John",10),
+        (2,"Alex",3),
+        (3,"Bob",14),
+        (4,"George",8));
