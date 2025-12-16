fbab61d9ca92a8592267de9a0c7734191ccb4d91
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-16 14:49:29 +0400

    Done

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- a script that creates a table called first_table in the current database
 -- a script that creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS(ID INT,
+CREATE TABLE IF NOT EXISTS first_table(ID INT,
               NAME VARCHAR(256));
