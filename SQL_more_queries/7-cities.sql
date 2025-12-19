289da32ca9d0899bf2d126d8a5e3be27155edcdf
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-19 10:43:00 +0400

    Done

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,7 +5,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
-    id int not null auto_increment primary key,
+    id INT NOT NULL AUTO_INCREMENT,
     state_id int not null FOREIGN KEY (state_id) REFERENCES states(id),
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
