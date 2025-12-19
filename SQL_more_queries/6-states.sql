a07d790855248fa39816fd52e10eea33f3da648c
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-19 10:29:47 +0400

    Done

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 -- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
 (
     id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL ,
