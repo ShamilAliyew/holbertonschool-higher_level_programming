3db95bf70fc61d8970da632fc098e5a7b31f78e9
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-19 10:34:49 +0400

    Done

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
 -- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 -- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
-(
-    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY ,
-    name VARCHAR(255) NOT NULL
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
