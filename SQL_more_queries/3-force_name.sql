610a5643d1c749f68c2b6a1cbeec7be0481f8b19
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-19 10:18:56 +0400

    Done

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- a script that creates the table force_name
+-- a script that creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name
+(
+    ID INT,
+    NAME VARCHAR(255) NOT NULL
+);
