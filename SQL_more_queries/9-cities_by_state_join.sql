49ae51c927855a07562da79f31621516772ca347
ShamilAliyew <shamil.aliyev005@gmail.com>
2025-12-21 16:55:17 +0400

    Done

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,8 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa
 -- a script that lists all cities contained in the database hbtn_0d_usa
 SELECT
-    C.ID,
-    C.NAME,
-    S.NAME
+    C.id,
+    C.name,
+    S.name
 FROM cities C
 JOIN states S ON C.state_id = S.id;
